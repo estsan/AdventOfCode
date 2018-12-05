36ca117005a9d7a6043892f6bf91bf0a8262c53c
Unknown <sandstrom.ester@gmail.com>
2018-12-05 09:23:57 +0100

    .

---
+++ FirstDoor/FirstDoor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FirstDoor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = File.ReadAllLines("TextFile1.txt");
+            int sum = 0;
+            foreach(string n in number)
+            {
+                if (n.Substring(0, 1) == "+")
+                {
+                    sum += int.Parse(n.Substring(1));
+                }
+                else
+                {
+                    sum -= int.Parse(n.Substring(1));
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
