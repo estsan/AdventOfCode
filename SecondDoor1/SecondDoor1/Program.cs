5f093c3646f31ed44637fc8814ba1835c645760e
Unknown <sandstrom.ester@gmail.com>
2018-12-05 14:13:23 +0100

    Done

--- SecondDoor1/SecondDoor1/Program.cs
+++ SecondDoor1/SecondDoor1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SecondDoor1
 {
@@ -10,6 +11,35 @@ namespace SecondDoor1
     {
         static void Main(string[] args)
         {
+            string[] boxIDs = File.ReadAllLines("TextFile.txt");
+            int two = 0;
+            int three = 0;
+            foreach (string s in boxIDs)
+            {
+                Dictionary<char, int> characterCount = new Dictionary<char, int> { };
+                foreach(char c in s)
+                {
+                    if (characterCount.ContainsKey(c))
+                    {
+                        characterCount[c]++;
+                    }
+                    else
+                    {
+                        characterCount[c] = 1;
+                    }
+                }
+                if (characterCount.ContainsValue(2))
+                {
+                    two++;
+                }
+                if (characterCount.ContainsValue(3))
+                {
+                    three++;
+                }
+            }
+            int checkcount = two * three;
+            Console.WriteLine(checkcount);
+            Console.ReadKey();
         }
     }
 }
