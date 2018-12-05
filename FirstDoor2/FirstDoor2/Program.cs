6884da8df64c0f002125e22b32da8367c4ec15d6
Unknown <sandstrom.ester@gmail.com>
2018-12-05 13:13:32 +0100

    Done

--- FirstDoor2/FirstDoor2/Program.cs
+++ FirstDoor2/FirstDoor2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FirstDoor2
 {
@@ -6,7 +9,37 @@ namespace FirstDoor2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] numbers = File.ReadAllLines("TextFile1.txt");
+            int frequency = 0;
+            List<int> frequencyList = new List<int> { };
+            bool forts = true;
+            int i = 0;
+            while (forts)
+            {
+                if (numbers[i].Substring(0,1) == "+")
+                {
+                    frequency += int.Parse(numbers[i].Substring(1));
+                }
+                else
+                {
+                    frequency -= int.Parse(numbers[i].Substring(1));
+                }
+                frequencyList.Add(frequency);
+                for (int j = 0; j < frequencyList.Count() - 1; j++)
+                {
+                    if (frequency == frequencyList[j])
+                    {
+                        forts = false;
+                    }
+                }
+                i++;
+                if (i >= numbers.Length)
+                {
+                    i = 0;
+                }
+            }
+            Console.WriteLine(frequency);
+            Console.ReadKey();
         }
     }
 }
